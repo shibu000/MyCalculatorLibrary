357d3666c0bbc3ee4c0f91cb8aa0610ca9a4219c
Shibu <shibu.prasad@razorpay.com>
2023-06-11 13:17:53 +0530

    Initial commit

---
+++ src/main/java/com/example/calculatorlib/MyCalculator.kt
@@ -0,0 +1,13 @@
+package com.example.calculatorlib
+
+object MyCalculator {
+
+    val add: (Double, Double) -> Double = { a,b -> a+b }
+
+    val subtract: (Double, Double) -> Double = { a,b -> a-b }
+
+    val multiply: (Double, Double) -> Double = { a,b -> a*b }
+
+    val divide: (Double, Double) -> Double = { a,b -> a/b }
+
+}
